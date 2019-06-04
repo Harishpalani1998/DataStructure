4339d1952640be15036b8b9e2d3ee21c223a44f2
Administrator <Administrator@DESKTOP-DROK7R8>
2019-06-04 16:29:20 +0530

    corrections

--- DataStructure/src/array/BubbleSort.java
+++ DataStructure/src/array/BubbleSort.java
@@ -9,23 +9,19 @@ public class BubbleSort {
             for (int j = 0; j < n-i-1; j++) 
                 if (arr[j] > arr[j+1]) 
                 { 
-                    // swap arr[j+1] and arr[i] 
+                    
                     int temp = arr[j]; 
                     arr[j] = arr[j+1]; 
                     arr[j+1] = temp; 
                 } 
     } 
-  
-    /* Prints the array */
     void printArray(int arr[]) 
     { 
         int n = arr.length; 
         for (int i=0; i<n; ++i) 
             System.out.print(arr[i] + " "); 
         System.out.println(); 
-    } 
-  
-    // Driver method to test above 
+    }  
     public static void main(String args[]) 
     { 
     	Scanner sc = new Scanner(System.in);
--- DataStructure/src/array/InsertionSort.java
+++ DataStructure/src/array/InsertionSort.java
@@ -9,18 +9,18 @@ public class InsertionSort {
 	        {
 	            int j = i;
 
-	            //i is not the first element
+	           
 	            while(j>0)
 	            {
-	                //not in order
+	                
 	                if(a[j-1] > a[j])
 	                {
-	                    //swapping
+	                    
 	                    int temp = a[j-1];
 	                    a[j-1] = a[j];
 	                    a[j] = temp;
 	                }
-	                //in order
+	                
 	                else
 	                {
 	                    break;
