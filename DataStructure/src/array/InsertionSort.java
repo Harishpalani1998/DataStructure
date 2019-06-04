bdb8ddd0aea4fcec5571d0f4b131b10835df7eda
Administrator <Administrator@DESKTOP-DROK7R8>
2019-06-04 18:21:55 +0530

    Selection sort for ascending order

--- DataStructure/src/array/InsertionSort.java
+++ DataStructure/src/array/InsertionSort.java
@@ -1,26 +1,19 @@
 package array;
-
 public class InsertionSort {
-
 	 public static void main (String[] args)
 	    {
 	        int a[] = {16, 19, 11, 15, 10, 12, 14};
 	        for(int i=0;i<a.length;i++)
 	        {
 	            int j = i;
-
-	           
 	            while(j>0)
 	            {
-	                
 	                if(a[j-1] > a[j])
-	                {
-	                    
+	                {   
 	                    int temp = a[j-1];
 	                    a[j-1] = a[j];
 	                    a[j] = temp;
-	                }
-	                
+	                }           
 	                else
 	                {
 	                    break;
@@ -28,12 +21,9 @@ public class InsertionSort {
 	                j--;
 	            }
 	        }
-
 	        for(int x : a)
 	        {
 	            System.out.println(x);
 	        }
-
 	    }
-
-}
+	}
